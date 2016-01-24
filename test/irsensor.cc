26410245c1356ccff65eb5fb5c254ca44612465b
Meghann Pi <meg@meghannpalmer.com>
2016-01-24 01:01:51 +0000

    Add C++ code to test IR sensors.

--- test/irsensor.cc
+++ test/irsensor.cc
@@ -3,30 +3,36 @@
 
 #include <wiringPi.h>
 
+using namespace std;
+
+int i, j, k;
+
 int main() 
 {
   wiringPiSetup(); // Initializes wiringPi using wiringPi's simlified number system.
   wiringPiSetupGpio(); // Initializes wiringPi using the Broadcom GPIO pin numbers
   
-  pinMode(3, INPUT);
-  pinMode(16, INPUT);
+  pinMode(2, INPUT);
+  pinMode(23, INPUT);
   
   // while loop with a single statement
   int i = 0;
   while (1)
     {
-      i = pinMode(3, OUTPUT);
-      j = pinMode(16, OUTPUT);
-     
+      i = digitalRead(2);
+      j = digitalRead(23);
+
       if (i == 0) 
 	{
-	  cout << "Obstacle detected on Left" << i << endl;
-	  usleep(1000);
+	  cout << "Obstacle detected on Left " << i << endl;
+	  delay(2000);
 	}
       else if (j == 0)
 	{
-	  cout << "Obstacle detected on Right" << j << endl;
-	  usleep(1000);
+	  cout << "Obstacle detected on Right  " << j << endl;
+	  delay(2000);
 	}
     }
+
+  return 0;
 }
